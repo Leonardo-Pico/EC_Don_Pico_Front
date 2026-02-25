c9284140c27be3f7f99615e0a3709f1604093d9f
Leonardo-Pico <63673074+Leonardo-Pico@users.noreply.github.com>
2026-02-25 17:50:53 -0500

    Update api.js

--- src/config/api.js
+++ src/config/api.js
@@ -1,5 +1,12 @@
 const API_URL = import.meta.env.VITE_API_URL;
 
+useEffect(() => {
+    fetch(`${API}/productos`)
+        .then(res => res.json())
+        .then(data => setProductos(data))
+        .catch(err => console.error(err));
+}, []);
+
 export const api = {
     getProducts: async (categoria = '', busqueda = '') => {
         const params = new URLSearchParams();
