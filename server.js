6f791ee5620ea0988d2f35a1dddc17ca5eb70f7b
Leonardo-Pico <63673074+Leonardo-Pico@users.noreply.github.com>
2026-02-27 11:24:07 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,20 @@
+const allowedOrigins = [
+    "http://localhost:5173",
+    "https://ec-don-pico-front.vercel.app"
+];
+
 app.use(cors({
-    origin: true,
+    origin: function (origin, callback) {
+        if (!origin) return callback(null, true);
+
+        if (
+            allowedOrigins.includes(origin) ||
+            origin.endsWith(".vercel.app")
+        ) {
+            callback(null, true);
+        } else {
+            callback(new Error("No permitido por CORS"));
+        }
+    },
     credentials: true
 }));
